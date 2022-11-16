639abf381e35c9c259ee908b7f6e634968caf0aa
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-11-16 17:31:06 +0100

    refactor: rename html classes to significant names

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ const comment = document.querySelector('.directors__comment');
 toggleComments.forEach((toggleCommentButton) => {
   toggleCommentButton.addEventListener('click', () => {
     toggleCommentButton.classList.toggle('close-button');
-    // overlay.classList.toggle('comment-display');
   });
+});
+
+// Responsive menu
+
+const heroMenu = document.querySelector('.header__menu');
+const toggleMenu = document.querySelector('.header__toggle-menu');
+
+toggleMenu.addEventListener('click', () => {
+  const visibility = heroMenu.getAttribute('data-visible');
+  if (visibility === 'false') {
+    heroMenu.setAttribute('data-visible', 'true');
+    toggleMenu.setAttribute('aria-expanded', 'true');
+  } else {
+    heroMenu.setAttribute('data-visible', 'false');
+    toggleMenu.setAttribute('aria-expanded', 'false');
+  }
 });
