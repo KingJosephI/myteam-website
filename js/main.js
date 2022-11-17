a90331696d95b12c55983d031182cf3548a8b092
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-11-17 03:08:03 +0100

    feat: create a mobile navigation

--- js/main.js
+++ js/main.js
@@ -9,16 +9,23 @@ toggleComments.forEach((toggleCommentButton) => {
 
 // Responsive menu
 
-const heroMenu = document.querySelector('.header__menu');
-const toggleMenu = document.querySelector('.header__toggle-menu');
+// const heroMenu = document.querySelector('.header__menu');
+// const toggleMenu = document.querySelector('.header__toggle-menu');
 
-toggleMenu.addEventListener('click', () => {
-  const visibility = heroMenu.getAttribute('data-visible');
-  if (visibility === 'false') {
-    heroMenu.setAttribute('data-visible', 'true');
-    toggleMenu.setAttribute('aria-expanded', 'true');
-  } else {
-    heroMenu.setAttribute('data-visible', 'false');
-    toggleMenu.setAttribute('aria-expanded', 'false');
-  }
+// toggleMenu.addEventListener('click', () => {
+//   const visibility = heroMenu.getAttribute('data-visible');
+//   if (visibility === 'false') {
+//     heroMenu.setAttribute('data-visible', 'true');
+//     toggleMenu.setAttribute('aria-expanded', 'true');
+//   } else {
+//     heroMenu.setAttribute('data-visible', 'false');
+//     toggleMenu.setAttribute('aria-expanded', 'false');
+//   }
+// });
+
+const hamburger = document.querySelector('.header__toggle-menu');
+const navBar = document.querySelector('.header__navigation');
+hamburger.addEventListener('click', () => {
+  navBar.classList.toggle('active');
+  hamburger.classList.toggle('close');
 });
